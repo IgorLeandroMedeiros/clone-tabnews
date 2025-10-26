89938a24564163a01d7b37024d9c12d8ec2ccdb2
Igor Leandro Dantas Medeiros <igtag.code@gmail.com>
2025-10-25 21:34:13 -0300

    lista de locais para ir com Nic!

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,10 @@ function Home() {
   return (
     <>
       <h1>Oi Nicolle eu amo você, se você me ama da uma risadinha 😁</h1>
+      <h3>Lugares para ir com Nic!</h3>
+      <ul>
+        <li>Aquário Paraiba & Espaço das Aves</li>
+      </ul>
     </>
   );
 }
